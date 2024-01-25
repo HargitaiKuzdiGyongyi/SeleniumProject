46234aadba18ed9dab88fc704c9e9c2e2a62b86a
HargitaiKuzdiGyongyi <98902271+HargitaiKuzdiGyongyi@users.noreply.github.com>
2024-01-25 21:22:25 +0100

    Add files via upload

---
+++ src/test/java/hu/masterfield/pages/BasePage.java
@@ -0,0 +1,84 @@
+package hu.masterfield.pages;
+
+
+import hu.masterfield.utils.GlobalTestData;
+import hu.masterfield.utils.Screenshot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * A Page POM osztályaink innen fogják örökölni az osztályban megvalósított metódusokat.
+ * Deklarálja a WebDriver-t, WebDriverWait-et, inicializálja az objektumokat, így ezeket nem kell
+ * külön megtennünk minden egyes osztályban.
+ */
+
+public class BasePage {
+
+    protected static Logger logger = LogManager.getLogger(BasePage.class);
+
+    protected static WebDriver driver;
+
+    protected static WebDriverWait wait;
+
+    protected static GlobalTestData globalTestData = new GlobalTestData();
+
+    public BasePage(WebDriver driver) {
+        BasePage.driver = driver;
+        BasePage.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean isLoaded(WebElement webElement) {
+        try {
+            return wait.until(ExpectedConditions.visibilityOf(webElement)).isDisplayed();
+        } catch (TimeoutException ex) {
+            fail(ex.getMessage());
+            return false;
+        }
+    }
+
+    public boolean isInteractable(WebElement webElement) {
+        try {
+            return wait.until(ExpectedConditions.elementToBeClickable(webElement)).isEnabled();
+        } catch (TimeoutException ex) {
+            fail(ex.getMessage());
+            return false;
+        }
+    }
+
+    public static void takesScreenshot() {
+        try {
+            Screenshot.takesScreenshot(driver);
+        } catch (IOException ex) {
+            logger.warn("BasePage.takesScreenshot was thrown IOException: " + ex.getMessage() +  " , "
+            + ex.getStackTrace());
+        }
+    }
+
+    public void setTextbox(WebElement webElement, String webElementName,String text) {
+        if (webElement.getText().isEmpty()) {
+            // TO DO NOTHING
+        } else {
+            try {
+                webElement.clear();
+                logger.trace(webElementName + ".clear() called");
+            } catch (Exception ex) {
+                logger.warn(webElementName + " textbox cannot clear.");
+            }
+        }
+        webElement.sendKeys(text);
+        logger.trace(webElementName + ".sendKeys() called");
+    }
+
+}
